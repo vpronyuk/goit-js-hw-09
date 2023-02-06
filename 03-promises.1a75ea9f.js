1d2e0ed513bb7174d9def4b534c79ae968eaf33a
vpronyuk <111579555+vpronyuk@users.noreply.github.com>
2023-02-06 20:47:06 +0000

    Deploying to gh-pages from @ vpronyuk/goit-js-hw-09@ea09402d5d27f0c09cb6a543656283c331006f58 🚀

--- 03-promises.1a75ea9f.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault(),position,delay,Math.random()}));
-//# sourceMappingURL=03-promises.1a75ea9f.js.map
--- 03-promises.b01a009a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault(),position,delay,Math.random()}));
-//# sourceMappingURL=03-promises.b01a009a.js.map
